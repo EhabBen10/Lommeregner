4b2fb14f994fb3fcd315cc1563606b850b1bedf4
Ehabben10 <202107666@post.au.dk>
2023-02-01 13:24:17 +0100

    Initial commit

---
+++ CalculatorTest/CalculatorUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using LommeRegner;
+
+namespace CalculatorTest;
+
+public class CalculatorUnitTests
+{
+    private Calculator Calculator;
+    [SetUp]
+    public void Setup()
+    {
+        Calculator = new Calculator();
+    }
+
+    [TestCase(2,2,4)]
+    [TestCase(2, 3, 5)]
+    [TestCase(-2, 3, 1)]
+    public void Add_Test(double a, double b, double resulte)
+    {
+
+        Assert.That(Calculator.Add(a, b), Is.EqualTo(resulte));
+    }
+    [TestCase(2, 1, 1)]
+    [TestCase(2, 3, -1)]
+    [TestCase(-2, 3, -5)]
+    public void Subtract_Test(double a, double b, double resulte)
+    {
+
+        Assert.That(Calculator.Subtract(a, b), Is.EqualTo(resulte));
+    }
+
+    [TestCase(2, 2, 4)]
+    [TestCase(2, 3, 6)]
+    [TestCase(-2, 3, -6)]
+    public void Multiply_Test(double a, double b, double resulte)
+    {
+
+        Assert.That(Calculator.Multiply(a, b), Is.EqualTo(resulte));
+    }
+    [TestCase(2, 2, 4)]
+    [TestCase(2, 3, 8)]
+    [TestCase(-2, 3, -8)]
+    public void Power_Test(double a, double b, double resulte)
+    {
+
+        Assert.That(Calculator.Power(a, b), Is.EqualTo(resulte));
+    }
+
+}
---
+++ LommeRegner/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LommeRegner
+{
+	public class Calculator
+	{
+		public Calculator()
+		{
+		}
+
+		public double Add(double a, double b)
+		{
+
+			return a + b;
+		}
+
+        public double Subtract(double a, double b)
+        {
+
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+
+            return a * b;
+        }
+
+        public double Power(double a, double exp)
+        {
+
+            return Math.Pow(a,exp);
+        }
+
+    }
+}
+
